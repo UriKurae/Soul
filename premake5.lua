8b5586ed0d23cf47839f31083c72076eea5a34ac
Oriol Bernal Martinez <youtfrost@gmail.com>
2022-10-07 20:23:04 +0200

    added premake
    added premake and a generate bat file to generate the project

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Soul"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+    project "Soul"
+        location "Soul"
+        kind "SharedLib"
+        language "C++"
+
+        targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "%{prj.name}/src/**.h",
+            "%{prj.name}/src/**.cpp",
+        }
+
+        includedirs
+        {
+            "%{prj.name}/vendor/spdlog/include"
+        }
+
+        filter "system:windows"
+            cppdialect "C++17"
+            staticruntime "On"
+            systemversion "latest"
+
+            defines
+            {
+                "SL_PLATFORM_WINDOWS",
+                "SL_BUILD_DLL"
+            }
+
+            postbuildcommands
+            {
+                ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+            }
+
+            filter "configurations:Debug"
+                defines "SL_DEBUG"
+                symbols "On"
+
+            filter "configurations:Release"
+                defines "SL_RELEASE"
+                optimize "On"
+
+            filter "configurations:Dist"
+                defines "SL_DIST"
+                optimize "On"
+
+project "Sandbox"
+            location "Sandbox"
+            kind "ConsoleApp"
+            language "C++"
+
+            targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+            objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+        files
+        {
+            "%{prj.name}/src/**.h",
+            "%{prj.name}/src/**.cpp",
+        }
+
+        includedirs 
+        {
+            "Soul/vendor/spdlog/include",
+            "Soul/src"
+        }
+
+        links
+        {
+            "Soul"
+        }
+
+        filter "system:windows"
+            cppdialect "C++17"
+            staticruntime "On"
+            systemversion "latest"
+
+            defines
+            {
+                "SL_PLATFORM_WINDOWS"
+            }
+
+            filter "configurations:Debug"
+                defines "SL_DEBUG"
+                symbols "On"
+
+            filter "configurations:Release"
+                defines "SL_RELEASE"
+                optimize "On"
+
+            filter "configurations:Dist"
+                defines "SL_DIST"
+                optimize "On"
