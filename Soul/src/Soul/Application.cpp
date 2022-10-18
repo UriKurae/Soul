788094346d58f08f4d30e401f75eb660485d0834
Oriol Bernal Martinez <youtfrost@gmail.com>
2022-10-18 17:33:50 +0200

    Window Events
    Added window events, such as closing the window, resizing, mouse callbacks, etc...

--- Soul/src/Soul/Application.cpp
+++ Soul/src/Soul/Application.cpp
@@ -2,19 +2,28 @@
 #include "Application.h"
 
 
-#include "Soul/Events/ApplicationEvent.h"
-
 #include <GLFW/glfw3.h>
 
 namespace Soul
 {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		SL_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -25,6 +34,12 @@ namespace Soul
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
 
--- Soul/src/Soul/Application.h
+++ Soul/src/Soul/Application.h
@@ -2,6 +2,9 @@
 
 #include "Core.h"
 #include "Soul/Events/Event.h"
+#include "Soul/Events/ApplicationEvent.h"
+
+
 #include "Window.h"
 
 namespace Soul
@@ -13,7 +16,11 @@ namespace Soul
 		virtual ~Application();
 
 		void Run();
+
+		void OnEvent(Event& e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
+
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 	};
