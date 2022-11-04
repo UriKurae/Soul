6c5c94547330372dc21dadec2499374a3163dace
Oriol Bernal Martinez <youtfrost@gmail.com>
2022-11-04 19:14:03 +0100

    Vertex buffer layouts abstracted
    Instead of having to write multiple times and lines the different vertex attributes, we now only have to set a layout and send it to the glVertexAttribPointer.

--- Soul/src/Soul/Application.cpp
+++ Soul/src/Soul/Application.cpp
@@ -14,6 +14,27 @@ namespace Soul
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case Soul::ShaderDataType::Float:		return GL_FLOAT;
+		case Soul::ShaderDataType::Float2:		return GL_FLOAT;
+		case Soul::ShaderDataType::Float3:		return GL_FLOAT;
+		case Soul::ShaderDataType::Float4:		return GL_FLOAT;
+		case Soul::ShaderDataType::Mat3:		return GL_FLOAT;
+		case Soul::ShaderDataType::Mat4:		return GL_FLOAT;
+		case Soul::ShaderDataType::Int:			return GL_INT;
+		case Soul::ShaderDataType::Int2:		return GL_INT;
+		case Soul::ShaderDataType::Int3:		return GL_INT;
+		case Soul::ShaderDataType::Int4:		return GL_INT;
+		case Soul::ShaderDataType::Bool:		return GL_BOOL;
+		}
+
+		SL_CORE_ASSERT(false, "Unknown ShaderDataType");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		SL_CORE_ASSERT(!s_Instance, "Application already exists");
@@ -28,18 +49,36 @@ namespace Soul
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] =
+		float vertices[3 * 7] =
 		{
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f,
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
+		{
+			BufferLayout layout = {
+				{ShaderDataType::Float3, "a_Position"},
+				{ShaderDataType::Float4, "a_Color"}
+			};
+			m_VertexBuffer->SetLayout(layout);
+		}
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 
 		uint32_t indices[3] = { 0,1,2 };
 
@@ -50,12 +89,15 @@ namespace Soul
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 			
 			out vec3 v_Position;		
+			out vec4 v_Color;		
 	
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 
@@ -67,10 +109,12 @@ namespace Soul
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
