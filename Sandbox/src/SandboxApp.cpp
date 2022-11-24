4f784c8908b5ee2c8f27da609d4b01628f4e6bc8
Oriol Bernal Martinez <youtfrost@gmail.com>
2022-11-24 17:09:26 +0100

    Changed some raw pointers to shared pointers
    Just so I can avoid having future memory leaks, I swapped some raw pointers to shared pointers.

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -13,30 +13,6 @@ public:
 	ExampleLayer()
 		: Layer("Example")
 	{		
-		m_VertexArray = Soul::VertexArray::Create();
-
-		float vertices[3 * 7] =
-		{
-			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
-			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
-			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f,
-		};
-
-		Soul::Ref<Soul::VertexBuffer> vertexBuffer;
-		vertexBuffer.reset(Soul::VertexBuffer::Create(vertices, sizeof(vertices)));
-		Soul::BufferLayout layout = {
-			{Soul::ShaderDataType::Float3, "a_Position"},
-			{Soul::ShaderDataType::Float4, "a_Color"}
-		};
-
-		vertexBuffer->SetLayout(layout);
-		m_VertexArray->AddVertexBuffer(vertexBuffer);
-
-		uint32_t indices[3] = { 0, 1, 2 };
-		Soul::Ref<Soul::IndexBuffer> indexBuffer;
-		indexBuffer.reset(Soul::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
-		m_VertexArray->SetIndexBuffer(indexBuffer);
-
 		m_SquareVA = Soul::VertexArray::Create();
 		float squareVertices[5 * 4] =
 		{
@@ -47,7 +23,7 @@ public:
 		};
 
 		Soul::Ref<Soul::VertexBuffer> squareVB;
-		squareVB.reset(Soul::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
+		squareVB = Soul::VertexBuffer::Create(squareVertices, sizeof(squareVertices));
 		squareVB->SetLayout({
 			{ Soul::ShaderDataType::Float3, "a_Position" },
 			{ Soul::ShaderDataType::Float2, "a_TexCoord" }
@@ -56,75 +32,8 @@ public:
 
 		uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
 		Soul::Ref<Soul::IndexBuffer> squareIB;
-		squareIB.reset((Soul::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t))));
+		squareIB = Soul::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t));
 		m_SquareVA->SetIndexBuffer(squareIB);
-
-		std::string vertexSrc = R"(
-			#version 330 core
-
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec4 a_Color;
-			
-			out vec3 v_Position;		
-			out vec4 v_Color;		
-	
-			void main()
-			{
-				v_Position = a_Position;
-				v_Color = a_Color;
-				gl_Position = vec4(a_Position, 1.0);
-			}
-
-		)";
-
-		std::string fragmentSrc = R"(
-			#version 330 core
-
-			layout(location = 0) out vec4 color;
-
-			in vec3 v_Position;
-			in vec4 v_Color;
-
-			void main()
-			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
-				color = v_Color;
-			}
-		)";
-
-		m_Shader = Soul::Shader::Create("VertexPosColor", vertexSrc, fragmentSrc);
-
-		std::string flatColorShaderVertexSrc = R"(
-			#version 330 core
-
-			layout(location = 0) in vec3 a_Position;
-			
-			out vec3 v_Position;		
-	
-			void main()
-			{
-				v_Position = a_Position;
-				gl_Position = vec4(a_Position, 1.0);
-			}
-
-		)";
-
-		std::string flatColorShaderFragmentSrc = R"(
-			#version 330 core
-
-			layout(location = 0) out vec4 color;
-
-			in vec3 v_Position;
-			
-			uniform vec3 u_Color;	
-	
-			void main()
-			{
-				color = vec4(u_Color, 1.0);
-			}
-		)";
-
-		m_FlatColorShader = Soul::Shader::Create("FlatColor", flatColorShaderVertexSrc, flatColorShaderFragmentSrc);
 
 		auto textureShader = m_ShaderLibrary.Load("assets/shaders/Texture.glsl");
 
@@ -163,10 +72,6 @@ public:
 
 		m_ZoroTexture->Bind();
 		Soul::Renderer::Submit(textureShader, m_SquareVA);
-
-
-		//std::dynamic_pointer_cast<Soul::OpenGLShader>(m_Shader)->Bind();
-		//Soul::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Soul::Renderer::EndScene();
 	}
--- Soul/src/Soul/Renderer/Buffer.cpp
+++ Soul/src/Soul/Renderer/Buffer.cpp
@@ -8,24 +8,24 @@
 
 namespace Soul
 {
-	VertexBuffer* VertexBuffer::Create(float* vertices, uint32_t size)
+	Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size)
 	{
 		switch (Renderer::GetAPI())
 		{
 			case RendererAPI::API::None:		SL_CORE_ASSERT(false, "None is not supported"); return nullptr;
-			case RendererAPI::API::OpenGL:		return new OpenGLVertexBuffer(vertices, size);
+			case RendererAPI::API::OpenGL:		return std::make_shared<OpenGLVertexBuffer>(vertices, size);
 		}
 
 		SL_CORE_ASSERT(false, "Unknown API");
 		return nullptr;
 	}
 
-	IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t size)
+	Ref<IndexBuffer> IndexBuffer::Create(uint32_t* indices, uint32_t size)
 	{
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None:		SL_CORE_ASSERT(false, "None is not supported"); return nullptr;
-		case RendererAPI::API::OpenGL:		return new OpenGLIndexBuffer(indices, size);
+		case RendererAPI::API::OpenGL:		return std::make_shared<OpenGLIndexBuffer>(indices, size);
 		}
 
 		SL_CORE_ASSERT(false, "Unknown API");
